76a1b6bfb6711ad9c4c685c3bc8643406bdca41d
kei2100 <kei.arima.2100@gmail.com>
2025-03-09 13:18:09 +0900

    a

---
+++ spring/src/main/kotlin/com/github/kei2100/spring/controller/filter/FilterConfig.kt
@@ -0,0 +1,54 @@
+package com.github.kei2100.spring.controller.filter
+
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+
+@Configuration
+class FilterConfig {
+    @Bean
+    fun firstFilter(): Filter {
+        return FirstFilter()
+    }
+
+    @Bean
+    fun secondFilter(): Filter {
+        return SecondFilter()
+    }
+}
+
+// @Component で指定したフィルターが優先される
+@Component
+class PrioritizedFilter : Filter {
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        (res as? HttpServletResponse)?.let {
+            it.addHeader("X-Filter-Order", "PrioritizedFilter")
+        }
+        chain.doFilter(req, res)
+    }
+}
+
+// @Bean で指定した順番に提供される
+class FirstFilter : Filter {
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        (res as? HttpServletResponse)?.let {
+            it.addHeader("X-Filter-Order", "FirstFilter")
+        }
+        chain.doFilter(req, res)
+    }
+}
+
+// @Bean で指定した順番に提供される
+class SecondFilter : Filter {
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        (res as? HttpServletResponse)?.let {
+            it.addHeader("X-Filter-Order", "SecondFilter")
+        }
+        chain.doFilter(req, res)
+    }
+}
---
+++ spring/src/test/kotlin/com/github/kei2100/spring/controller/filter/FilterConfigTest.kt
@@ -0,0 +1,34 @@
+package com.github.kei2100.spring.controller.filter
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.RequestEntity
+import org.springframework.http.ResponseEntity
+import java.net.URI
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FilterConfigTest {
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
+
+    @LocalServerPort
+    private var port: Int = 0
+
+    @Test
+    fun `X-Filter-Orderの値が期待する順番で設定されていること`() {
+        val url = "http://localhost:$port/"
+        val requestEntity = RequestEntity<Any>(HttpHeaders(), HttpMethod.GET, URI(url))
+
+        // API 呼び出し
+        val response: ResponseEntity<String> = restTemplate.exchange(requestEntity, String::class.java)
+        // レスポンスヘッダーの確認
+        Assertions.assertThat(response.headers["X-Filter-Order"])
+            .contains("PrioritizedFilter", "FirstFilter", "SecondFilter")
+    }
+}
