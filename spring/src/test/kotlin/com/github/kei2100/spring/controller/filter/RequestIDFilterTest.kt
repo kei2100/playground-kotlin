55e50c16a7b4f4c001ea123bc4b61bf9245ecb6b
kei2100 <kei.arima.2100@gmail.com>
2025-03-09 12:22:57 +0900

    add

---
+++ spring/src/test/kotlin/com/github/kei2100/spring/controller/filter/RequestIDFilterTest.kt
@@ -0,0 +1,33 @@
+package com.github.kei2100.spring.controller.filter
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import java.util.*
+
+@WebMvcTest
+class HeaderTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `レスポンスヘッダーにX-Request-IDが含まれること`() {
+        mockMvc.get("/")
+            .andExpect {
+                header { exists("X-Request-ID") }  // ヘッダーが存在することを確認
+            }
+    }
+
+    @Test
+    fun `レスポンスヘッダーにリクエストヘッダーで指定した値のX-Request-IDが含まれること`() {
+        val rid = UUID.randomUUID().toString()
+        mockMvc.get("/") {
+            header("X-Request-ID", rid)
+        }
+            .andExpect {
+                header { string("X-Request-ID", rid) }
+            }
+    }
+}
